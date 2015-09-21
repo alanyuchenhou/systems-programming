5dc24ac3d91c2a330bb819bfdeb1b3878a12d77b
Yuchen Hou <houych@gmail.com>
2015-09-20 18:22:10 -0700

    finish lab3

--- src/lab3.c
+++ src/lab3.c
@@ -16,6 +16,7 @@
 #include <fcntl.h>
 
 int getCommand(char* arguments[]) {
+	printf("##############################################################\n");
 	printf("$ ");
 	char* line = NULL;
 	char* token = NULL;
@@ -47,21 +48,21 @@ int execute(char* arguments[]) {
 	for (; arguments[index] != NULL; index++) {
 		if (strcmp(arguments[index], "<") == 0) {
 //			printf("child: input redirection!\n");
-			int fdin = open(arguments[index+1], O_RDONLY);
+			int fdin = open(arguments[index + 1], O_RDONLY);
 			dup2(fdin, 0);
 			close(fdin);
 			break;
 		} else if (strcmp(arguments[index], ">") == 0) {
 //			printf("child: output redirection!\n");
-			FILE* garbage = fopen(arguments[index+1], "w+");
+			FILE* garbage = fopen(arguments[index + 1], "w+");
 			fclose(garbage);
-			int fdout = open(arguments[index+1], O_WRONLY | O_CREAT, 0600);
+			int fdout = open(arguments[index + 1], O_WRONLY | O_CREAT, 0600);
 			dup2(fdout, 1);
 			close(fdout);
 			break;
 		} else if (strcmp(arguments[index], ">>") == 0) {
 //			printf("child: output append redirection!\n");
-			int fdout = open(arguments[index+1], O_WRONLY | O_APPEND);
+			int fdout = open(arguments[index + 1], O_WRONLY | O_APPEND);
 			dup2(fdout, 1);
 			close(fdout);
 			break;
@@ -72,21 +73,57 @@ int execute(char* arguments[]) {
 	return 0;
 }
 
-int makeChildWork(char* arguments[]) {
+int buildPipe(char* arguments[], char* head[], char* tail[]) {
+	if (head == NULL && tail == NULL) {
+		execute(arguments);
+	} else {
+		int pipeDescriptor[2];
+		pipe(pipeDescriptor);
+		int pid = fork();
+		if (pid < 0) { // fork() may fail
+			perror("fork faild");
+			exit(1);
+		}
+		if (pid) { // parent read
+			close(pipeDescriptor[1]); // reader doesn't write;
+			dup2(pipeDescriptor[0], 0); // reader.stdin = pipe[0]
+			close(pipeDescriptor[0]);
+			execute(tail);
+		} else { // child write
+			close(pipeDescriptor[0]); // writer doesn't read
+			dup2(pipeDescriptor[1], 1); // writer.stdout = pipe[1]
+			close(pipeDescriptor[1]);
+			execute(head);
+		}
+	}
+	return 0;
+}
+
+int getChild(char* arguments[]) {
 //	printf("makeChildWork: initiating\n");
-	int pid;
 	int status;
-	pid = fork();
+	int pid = fork();
 	if (pid < 0) { // fork() may fail
 		perror("fork faild");
 		exit(1);
 	}
-	if (pid != 0) { // parent
-//		printf("parent: pid = %d; ppid = %d\n", getpid(), getppid());
+	if (pid) { // parent
+		printf("parent: pid = %d; ppid = %d\n", getpid(), getppid());
 		pid = wait(&status);
-//		printf("parent: dead_child.pid = %d, status = %04x\n", pid, status);
+		printf("parent: dead_child.pid = %d, status = %04x\n", pid, status);
 	} else { // child
-		execute(arguments);
+		int index = 0;
+		char** head = NULL;
+		char** tail = NULL;
+		for (; arguments[index] != NULL; index++) {
+			if (strcmp(arguments[index], "|") == 0) {
+				head = arguments;
+				arguments[index] = NULL;
+				tail = arguments + index + 1;
+				break;
+			}
+		}
+		buildPipe(arguments, head, tail);
 	}
 	return 0;
 }
@@ -105,7 +142,7 @@ int mysh() {
 	} else if (strcmp(arguments[0], "exit") == 0) {
 		exit(1);
 	} else {
-		makeChildWork(arguments);
+		getChild(arguments);
 	}
 	return 0;
 }
