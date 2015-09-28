4f7db0ea938f470774f29b2865213e616edae8c5
Yuchen Hou <houych@gmail.com>
2015-09-28 10:26:21 -0700

    finish lab4 part2

--- src/mycgi.c
+++ src/mycgi.c
@@ -23,26 +23,24 @@ int copy (FILE* source, FILE* destination) {
 	while (getline(&buffer, &length, source) != -1) {
 		fputs(buffer, destination);
 	}
+	free(buffer);
 	return 0;
 }
-int main4() {
+int main(int argc, char *argv[]) {
+	int i, m, r;
 	char cwd[128];
-	entry[0].name = "command";
-	entry[0].value = "ls";
-	entry[1].name = "file1";
-	entry[1].value = ".";
-	entry[2].name = "file2";
-	entry[2].value = "f1";
+
+	m = getinputs(); // get user inputs name=value into entry[ ]
 	getcwd(cwd, 128); // get CWD pathname
 
-//	printf("Content-type: text/html\n\n");
-//	printf("<p>pid=%d uid=%d cwd=%s\n", getpid(), getuid(), cwd);
-//	printf("<H1>Inputs</H1>");
-//	printf("You submitted the following name/value pairs:<p>");
-//
-//	for (i = 0; i <= m; i++)
-//		printf("%s = %s<p>", entry[i].name, entry[i].value);
-//	printf("<H1>Outputs</H1>");
+	printf("Content-type: text/html\n\n");
+	printf("<p>pid=%d uid=%d cwd=%s\n", getpid(), getuid(), cwd);
+	printf("<H1>Inputs</H1>");
+	printf("You submitted the following name/value pairs:<p>");
+
+	for (i = 0; i <= m; i++)
+		printf("%s = %s<p>", entry[i].name, entry[i].value);
+	printf("<H1>Outputs</H1>");
 	/*****************************************************************
 	 Write YOUR C code here to processs the command
 	 mkdir dirname
@@ -69,23 +67,41 @@ int main4() {
 		struct dirent* dirEntry;
 		if (dir) {
 			while ((dirEntry = readdir(dir)) != NULL) {
-//				printf("%s<p>", dirEntry->d_name);
-				printf("%s\n", dirEntry->d_name);
+				printf("%s<p>", dirEntry->d_name);
 			}
 			closedir(dir);
 			status = 0;
 		}
-	} else if (strcmp(entry[0].value, "cat") == 0) {
+	}
+	else if (strcmp(entry[0].value, "cat") == 0) {
 		FILE* source = fopen(entry[1].value, "r");
 		status = copy(source, stdout);
 		fclose(source);
-	} else if (strcmp(entry[0].value, "cp") == 0) {
+	}
+	else if (strcmp(entry[0].value, "cp") == 0) {
 		FILE* source = fopen(entry[1].value, "r");
 		FILE* destination = fopen(entry[2].value, "w+");
 		status = copy(source, destination);
 		fclose(source);
 		fclose(destination);
 	}
-	printf("status = %d\n", status);
+	printf("status = %d<p>", status);
+	// create a FORM webpage for user to submit again
+	printf("</title>");
+	printf("</head>");
+//  printf("<body bgcolor=\"#FF0000\" link=\"#330033\" leftmargin=8 topmargin=8");
+	printf("<p>------------------ DO IT AGAIN ----------------\n");
+	printf(
+			"<FORM METHOD=\"POST\" ACTION=\"http://cs360.eecs.wsu.edu/~hou/cgi-bin/mycgi\">");
+	printf("Enter command : <INPUT NAME=\"command\"> <P>");
+	printf("Enter filename1: <INPUT NAME=\"filename1\"> <P>");
+	printf("Enter filename2: <INPUT NAME=\"filename2\"> <P>");
+	printf(
+			"Submit command: <INPUT TYPE=\"submit\" VALUE=\"Click to Submit\"><P>");
+	printf("</form>");
+	printf("------------------------------------------------<p>");
+
+	printf("</body>");
+	printf("</html>");
 	return 0;
 }
